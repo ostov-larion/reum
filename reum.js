042c68b653a4b516431aa33a74c8ba11bc2de418
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2020-05-23 16:59:26 +0500

    Update

--- reum.js
+++ reum.js
@@ -4,6 +4,7 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
+// * Printing Functions
 say    = ([string]) => console.log('|',string) || true
 title  = ([string]) => console.log("| \x1b[47m\x1b[30m %s \x1b[0m",string) || true
 dialog = string => console.log("- \x1b[36m%s\x1b[0m",string) || true
@@ -11,31 +12,80 @@ help   = string => console.log("? \x1b[33m%s\x1b[0m",string) || true
 error  = string => console.log("! \x1b[31m%s\x1b[0m",string) || true
 list   = array  => array.map(e => console.log('*',e.name))
 
-onFailAction  = () => error("Ты не можешь это сделать по очевидным(и не очень) причинам.")
-onRetryAction = () => error("But you already do it...")
-onUnknownWord = () => error("Unknown command!")
-
+// * Register
 Rules = []
 Facts = new Set()
+Antonyms = []
+Synonyms = []
+grammar = {
+	not: 'not',
+	isPositive(sent){
+		return sent.indexOf(' '+grammar.not+' ') == -1 
+	},
+	isNegative(sent){
+		return !grammar.isPositive(sent)
+	},
+	toPositive(sent){
+		return sent.replace(grammar.not+' ','').trim()
+	},
+	toNegative(sent){
+		return grammar.not + ' ' + sent
+	}
+}
 
-rule = ([event]) => ([conds]) => ([action]) => ([postconds]) => Rules.push({event: new RegExp(event.trim()), conds: conds.split(',').map(e => e.trim()), action, postconds})
+
+// * Compute Function
+computeOpposition = fact => {
+	for({when,then} of Antonyms){
+		if(grammar.isNegative(fact) && new RegExp(when).test(fact)) return grammar.toPositive(fact.replace(when,then))
+		if(grammar.isNegative(fact) && new RegExp(then).test(fact)) return grammar.toPositive(fact.replace(then,when))
+		if(new RegExp(when).test(fact)) return grammar.toNegative(fact.replace(when,then))
+		if(new RegExp(then).test(fact)) return grammar.toNegative(fact.replace(then,when))
+	}
+}
+
+computeUnless = fact => {
+	for({when,then} of Synonyms){
+		when.every
+	}
+}
+
+// * Decorators
+afterEffect = (fn1,fn2) => new Proxy(fn1,{apply: (t,ctx,args) => (t.apply(ctx,args),fn2.apply(ctx,args))})
+
+// * Helpers
+splitComma = str => str.split(',').map(e => e.trim())
+from = from => ({to: fn => fn(from)})
+
+// * DSL
+rule = ([event]) => ([conds]) => ([action]) => ([postconds]) => Rules.push({event: new RegExp(event.trim()), conds: splitComma(conds), action, postconds})
 
 implement = ([event]) => {
 	for(rule of Rules){
 		rule.event.test(event)
 		&&
-		rule.conds.every(cond => cond.indexOf(' не ') == -1 ? Facts.has(cond) : !Facts.has(cond.replace(' не ',' ').trim()))
+		rule.conds.every(cond => grammar.isPositive(cond) ? Facts.has(cond) : !Facts.has(grammar.toPositive(cond)))
 		&&
 		say([rule.action])
 		&&
 		now([rule.postconds])
 	}
 }
-now = ([conds]) => conds.split(',').forEach(cond => cond.indexOf(' не ') == -1 ? Facts.add(cond.trim()) : !Facts.delete(cond.replace(' не ',' ').trim()))
-assert = now
-retract = ([cond]) => Facts.delete(cond)
+now = ([conds]) => splitComma(conds).forEach(cond => grammar.isPositive(cond) ? Facts.add(cond) : Facts.delete(grammar.toPositive(cond)))
+now = afterEffect(now,([conds]) => splitComma(conds).forEach(cond => {
+	let opp = computeOpposition(cond)
+	if(grammar.isNegative(cond)){
+		opp && Facts.add(opp)
+	}
+	else{
+		opp && Facts.delete(grammar.toPositive(opp))
+	}
+}))
 
+antonyms = ([when]) => ([then]) => Antonyms.push({when, then})
+synonyms = ([when]) => ([then]) => Synonyms.push({when, then})
 
+// * Main Loop
 function input(string){
 	implement([string])
 }
@@ -48,4 +98,4 @@ function start(){
 	});
 }
 
-module.exports = {prompt,start,rule,implement,now,assert,retract,say,error,help,title,dialog,error,list,Facts,Rules}
+module.exports = {prompt,start,rule,implement,now,synonyms,antonyms,say,error,help,title,dialog,error,list,Facts,Rules,grammar}
